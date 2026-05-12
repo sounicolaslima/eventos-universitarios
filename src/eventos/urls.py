0a75b85501d1cceb4605e17f17325e10f5bb5e8f
Rafaella <rafaellamacielleite17@gmail.com>
2026-05-12 12:45:41 -0300

    feat: adiciona geração de QR Code para compras

--- src/eventos/urls.py
+++ src/eventos/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -17,3 +19,6 @@ urlpatterns = [
     path('organizador/ingresso/editar/<int:ingresso_id>/', views.editar_ingresso, name='editar_ingresso'),
     path('organizador/ingresso/excluir/<int:ingresso_id>/', views.excluir_ingresso, name='excluir_ingresso'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
